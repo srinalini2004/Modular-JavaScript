42937b50ee7f9a01263cf8866df02fd765d17f52
srinalini2004 <160092260+srinalini2004@users.noreply.github.com>
2025-12-05 18:25:25 +0530

    Create footer.js

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+export function footer() {
+  return `
+    <footer style="margin-top:40px; padding:10px; background:#eee; text-align:center;">
+      <p>© 2025 Modular JS Project</p>
+    </footer>
+  `;
+}
